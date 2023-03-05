88007af90ec92137f6f26c1dd9e3ef8a22b53f0d
Nina <naneska-1@yandex.ru>
2023-03-05 20:11:22 +0300

    fix typo

--- hw2task1.java
+++ hw2task1.java
@@ -1,11 +1,10 @@
-// Задание 1. Дана строка sql-запроса "select * from students where ".
+public class hw2task1 {
+ // Задание 1. Дана строка sql-запроса "select * from students where ".
 // Сформируйте часть WHERE этого запроса, используя StringBuilder. 
 // Данные для фильтрации приведены ниже в виде json строки.
 // Если значение null, то параметр не должен попадать в запрос.
 // Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
-
-public class hw2task1 {
-    public static void main(String[] args) {
+public static void main(String[] args) {
         String json = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
         String sql = createSql(json);
         System.out.println(sql);
